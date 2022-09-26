ecb4c235a031fc5b91e7c6149e1164d6cd7377aa
EMILY MCEWEN <emmcewen@gmail.com>
2022-09-26 15:27:16 -0400

    finished webpack config file and src-sw.js file code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -57,9 +57,13 @@ module.exports = () => {
           test:/\.m?js$/,
           exclude: /node_modules/,
           use:{
-            loader:
-          }
-        }
+            loader:'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
         
       ],
     },
